c15b1ac573711a62dcc1aaeb479b8c4fcc4e36eb
ssj400 <gafetel2017@gmail.com>
2025-08-05 19:03:05 -0400

    Update CORS origin to use environment variable for flexibility

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
